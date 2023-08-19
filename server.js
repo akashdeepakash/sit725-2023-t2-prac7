65d0acbf818e8853184425a1721df7c0d49f408a
Akash <s223040483@deakin.edu.au>
2023-08-19 22:33:38 +1000

    practical5.2c
    mvc model

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
 let express = require('express');
 let app = express();
 let port = process.env.port || 3000;
+require('./dbConnection');
+let router = require('./routers/router');
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/cat',router);
 
-
-
-const cardList = [ 
-    {
-        title: "Kitten 2",
-        path: "catdog2.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        path: "C:\Users\x3161410\Desktop\catdog3.png",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
-    }
-];
-app.use(express.static(__dirname + '/'));
-app.set('view engine', 'ejs');
-
-app.get('/', function (req, res) {
-    res.render('pages/index', {cats:cardList});
-});
-
-
-app.listen(port, () => {
-    console.log("App listening to: " + port);
+app.listen(port, ()=>{
+    console.log('express server started');
 });
